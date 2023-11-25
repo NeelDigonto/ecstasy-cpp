eaa37aa9c2e01e2c656aa0ecab397320780d8529
Saikat Dey <saikatdey2100@gmail.com>
2023-11-25 14:53:49 +0530

    CMake Refactor.
    Signed-off-by: Saikat Dey <saikatdey2100@gmail.com>

--- ecstasy/src/InputController.cpp
+++ ecstasy/src/InputController.cpp
@@ -3,7 +3,7 @@
 #include <GLFW/glfw3.h>
 #include <ecstasy/ecstasy.hpp>
 
-ecstasy::InputController::InputController(GLFWwindow* _window,
+/* ecstasy::InputController::InputController(GLFWwindow* _window,
                                           Eigen::Vector2i _window_dimension) {
     int width, height;
     glfwGetWindowSize(_window, &width, &height);
@@ -51,102 +51,86 @@ ecstasy::InputController::InputController(GLFWwindow* _window,
         auto app = static_cast<ecstasy::app*>(glfwGetWindowUserPointer(window));
         app->getInputController()->accumulateScrollChange({xoffset, yoffset});
     });
-}
+} */
 
-void ecstasy::InputController::updateViewportDimension(
-    const Eigen::Vector2i& _viewport_dimension) {
+void ecstasy::InputController::updateViewportDimension(const Eigen::Vector2i& _viewport_dimension) {
     viewport_dimension_ = _viewport_dimension;
 }
 
-decltype(ecstasy::InputController::mbutton_state_)&
-ecstasy::InputController::getMButtonState() {
+decltype(ecstasy::InputController::mbutton_state_)& ecstasy::InputController::getMButtonState() {
     return mbutton_state_;
 }
 
-decltype(ecstasy::InputController::kbutton_state_)&
-ecstasy::InputController::getKButtonState() {
+decltype(ecstasy::InputController::kbutton_state_)& ecstasy::InputController::getKButtonState() {
     return kbutton_state_;
 }
 
-void ecstasy::InputController::updateCursorPos(
-    const Eigen::Vector2i& _new_pos) {
+void ecstasy::InputController::updateCursorPos(const Eigen::Vector2i& _new_pos) {
     for (auto& [_, cursor_pos_change] : cursor_pos_changes_) {
         cursor_pos_change.current_cursor_pos_ = _new_pos;
 
-        const auto diffx =
-            static_cast<float>(cursor_pos_change.current_cursor_pos_.x() -
-                               cursor_pos_change.last_animate_cursor_pos.x()) /
-            viewport_dimension_.x();
+        const auto diffx = static_cast<float>(cursor_pos_change.current_cursor_pos_.x() -
+                                              cursor_pos_change.last_animate_cursor_pos.x()) /
+                           viewport_dimension_.x();
 
-        const auto diffy =
-            static_cast<float>(cursor_pos_change.current_cursor_pos_.y() -
-                               cursor_pos_change.last_animate_cursor_pos.y()) /
-            viewport_dimension_.y();
+        const auto diffy = static_cast<float>(cursor_pos_change.current_cursor_pos_.y() -
+                                              cursor_pos_change.last_animate_cursor_pos.y()) /
+                           viewport_dimension_.y();
 
         cursor_pos_change.cursor_pos_diff_ = {diffx, diffy};
     }
 }
 
-void ecstasy::InputController::accumulateScrollChange(
-    const Eigen::Vector2i& _change) {
+void ecstasy::InputController::accumulateScrollChange(const Eigen::Vector2i& _change) {
 
     for (auto& [_, scroll_change] : scroll_changes_)
         scroll_change += _change;
 }
 
-ecstasy::InputController::SubscriberID
-ecstasy::InputController::registerCursorPosChangeUpdater() {
+ecstasy::InputController::SubscriberID ecstasy::InputController::registerCursorPosChangeUpdater() {
     const auto subscriber_id = cursor_pos_changes_.size();
-    cursor_pos_changes_.insert(
-        {subscriber_id, {current_cursor_pos_, current_cursor_pos_}});
+    cursor_pos_changes_.insert({subscriber_id, {current_cursor_pos_, current_cursor_pos_}});
 
     return subscriber_id;
 }
 
-void ecstasy::InputController::deregisterCursorPosChangeUpdater(
-    SubscriberID _subscriber_id) {
+void ecstasy::InputController::deregisterCursorPosChangeUpdater(SubscriberID _subscriber_id) {
     cursor_pos_changes_.erase(_subscriber_id);
 }
 
-ecstasy::InputController::SubscriberID
-ecstasy::InputController::registerScrollChangeAccumulator() {
+ecstasy::InputController::SubscriberID ecstasy::InputController::registerScrollChangeAccumulator() {
     const auto subscriber_id = cursor_pos_changes_.size();
     scroll_changes_.insert({subscriber_id, {0, 0}});
 
     return subscriber_id;
 }
 
-void ecstasy::InputController::deregisterScrollChangeAccumulator(
-    SubscriberID _subscriber_id) {
+void ecstasy::InputController::deregisterScrollChangeAccumulator(SubscriberID _subscriber_id) {
     scroll_changes_.erase(_subscriber_id);
 }
 
-ecstasy::CursorPosInfo&
-ecstasy::InputController::getCursorPosChange(SubscriberID _subscriber_id) {
+ecstasy::CursorPosInfo& ecstasy::InputController::getCursorPosChange(SubscriberID _subscriber_id) {
     const auto it = cursor_pos_changes_.find(_subscriber_id);
     assert(it != end(cursor_pos_changes_) && "Unkown Subscriber");
 
     return it->second;
 }
 
-void ecstasy::InputController::setCursorPosChange(SubscriberID _subscriber_id,
-                                                  const Eigen::Vector2i& _pos) {
+void ecstasy::InputController::setCursorPosChange(SubscriberID _subscriber_id, const Eigen::Vector2i& _pos) {
     const auto it = cursor_pos_changes_.find(_subscriber_id);
     assert(it != end(cursor_pos_changes_) && "Unkown Subscriber");
 
     it->second = {_pos, _pos};
 }
 
-Eigen::Vector2i&
-ecstasy::InputController::getScrollChange(SubscriberID _subscriber_id) {
+Eigen::Vector2i& ecstasy::InputController::getScrollChange(SubscriberID _subscriber_id) {
     const auto it = scroll_changes_.find(_subscriber_id);
     assert(it != end(scroll_change_) && "Unkown Subscriber");
 
     return it->second;
 }
 
-void ecstasy::InputController::setScrollChange(SubscriberID _subscriber_id,
-                                               const Eigen::Vector2i& _change) {
+void ecstasy::InputController::setScrollChange(SubscriberID _subscriber_id, const Eigen::Vector2i& _change) {
     const auto it = scroll_changes_.find(_subscriber_id);
     assert(it != end(scroll_changes_) && "Unkown Subscriber");
 
--- ecstasy/src/ecstasy.cpp
+++ ecstasy/src/ecstasy.cpp
@@ -54,14 +54,11 @@
 // #define GLFW_NATIVE_INCLUDE_NONE
 #include <GLFW/glfw3native.h>
 
-Eigen::IOFormat ecstasy::CommaInitFmt(Eigen::StreamPrecision,
-                                      Eigen::DontAlignCols, ", ", ", ", "", "",
+Eigen::IOFormat ecstasy::CommaInitFmt(Eigen::StreamPrecision, Eigen::DontAlignCols, ", ", ", ", "", "",
                                       " << ", ";");
 Eigen::IOFormat ecstasy::CleanFmt(4, 0, ", ", "\n", "[", "]");
-Eigen::IOFormat ecstasy::OctaveFmt(Eigen::StreamPrecision, 0, ", ", ";\n", "",
-                                   "", "[", "]");
-Eigen::IOFormat ecstasy::HeavyFmt(Eigen::FullPrecision, 0, ", ", ";\n", "[",
-                                  "]", "[", "]");
+Eigen::IOFormat ecstasy::OctaveFmt(Eigen::StreamPrecision, 0, ", ", ";\n", "", "", "[", "]");
+Eigen::IOFormat ecstasy::HeavyFmt(Eigen::FullPrecision, 0, ", ", ";\n", "[", "]", "[", "]");
 
 const static uint32_t indices[] = {0, 1, 2, 2, 3, 0};
 
@@ -82,8 +79,49 @@ const static filament::math::float3 vertices[] = {
 const static filament::math::short4 normals[]{tbn, tbn, tbn, tbn}; */
 
 // https : // github.com/BinomialLLC/basis_universal
-ecstasy::app::app(std::string _app_name, std::uint32_t _window_width,
-                  std::uint32_t _window_height) {
+
+ecstasy::InputController::InputController(GLFWwindow* _window, Eigen::Vector2i _window_dimension) {
+    int width, height;
+    glfwGetWindowSize(_window, &width, &height);
+    viewport_dimension_ = {width, height};
+
+    glfwSetKeyCallback(_window, [](GLFWwindow* window, int key, int scancode, int action, int mods) {
+        auto app = static_cast<ecstasy::app*>(glfwGetWindowUserPointer(window));
+        auto input_controller = app->getInputController();
+        input_controller->kbutton_state_[key] = action;
+        // fmt::print("{}\n", action);
+    });
+
+    glfwSetCharCallback(_window, [](GLFWwindow* window, unsigned int codepoint) {
+        auto app = static_cast<ecstasy::app*>(glfwGetWindowUserPointer(window));
+    });
+
+    glfwSetCharModsCallback(_window, [](GLFWwindow* window, unsigned int codepoint, int mods) {
+        auto app = static_cast<ecstasy::app*>(glfwGetWindowUserPointer(window));
+    });
+
+    glfwSetMouseButtonCallback(_window, [](GLFWwindow* window, int button, int action, int mods) {
+        auto app = static_cast<ecstasy::app*>(glfwGetWindowUserPointer(window));
+        auto input_controller = app->getInputController();
+        input_controller->mbutton_state_[button] = action;
+        // fmt::print("{}\n", action);
+    });
+
+    glfwSetCursorPosCallback(_window, [](GLFWwindow* window, double xposIn, double yposIn) {
+        auto app = static_cast<ecstasy::app*>(glfwGetWindowUserPointer(window));
+        app->getInputController()->current_cursor_pos_ = {
+            xposIn, app->getInputController()->viewport_dimension_.y() - yposIn};
+
+        app->getInputController()->updateCursorPos(app->getInputController()->current_cursor_pos_);
+    });
+
+    glfwSetScrollCallback(_window, [](GLFWwindow* window, double xoffset, double yoffset) {
+        auto app = static_cast<ecstasy::app*>(glfwGetWindowUserPointer(window));
+        app->getInputController()->accumulateScrollChange({xoffset, yoffset});
+    });
+}
+
+ecstasy::app::app(std::string _app_name, std::uint32_t _window_width, std::uint32_t _window_height) {
     app_name_ = _app_name;
     window_height_ = _window_height;
     window_width_ = _window_width;
@@ -93,29 +131,24 @@ ecstasy::app::app(std::string _app_name, std::uint32_t _window_width,
 
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-    window_ = glfwCreateWindow(window_width_, window_height_, app_name_.c_str(),
-                               NULL, NULL);
+    window_ = glfwCreateWindow(window_width_, window_height_, app_name_.c_str(), NULL, NULL);
     if (!window_)
         throw std::runtime_error("Could not open window!");
 
     glfwMakeContextCurrent(window_);
     glfwSetWindowUserPointer(window_, this);
 
-    input_controller_ =
-        new InputController(window_, {window_width_, window_height_});
+    input_controller_ = new InputController(window_, {window_width_, window_height_});
 
-    glfwSetFramebufferSizeCallback(window_, [](GLFWwindow* window, int width,
-                                               int height) {
+    glfwSetFramebufferSizeCallback(window_, [](GLFWwindow* window, int width, int height) {
         auto app = static_cast<ecstasy::app*>(glfwGetWindowUserPointer(window));
         app->getInputController()->updateViewportDimension({width, height});
     });
 
-    filament_engine_ = filament::Engine::create(
-        filament::Engine::Backend::OPENGL); // Engine::Backend::VULKAN
+    filament_engine_ = filament::Engine::create(filament::Engine::Backend::OPENGL); // Engine::Backend::VULKAN
 
     auto native_window = glfwGetWin32Window(window_);
-    filament_swapchain_ =
-        filament_engine_->createSwapChain((void*)native_window);
+    filament_swapchain_ = filament_engine_->createSwapChain((void*)native_window);
     renderer_ = filament_engine_->createRenderer();
 
     auto cameraEntity = utils::EntityManager::get().create();
@@ -127,18 +160,14 @@ ecstasy::app::app(std::string _app_name, std::uint32_t _window_width,
     view_->setScene(scene_);
     view_->setViewport({0, 0, window_width_, window_height_});
 
-    camera_->lookAt(filament::math::float3(0, 0, 50.f),
-                    filament::math::float3(0, 0, 0),
+    camera_->lookAt(filament::math::float3(0, 0, 50.f), filament::math::float3(0, 0, 0),
                     filament::math::float3(0, 1.f, 0));
-    camera_->setProjection(90.0,
-                           double(this->window_width_) / this->window_height_,
-                           0.1, 50, filament::Camera::Fov::VERTICAL);
+    camera_->setProjection(90.0, double(this->window_width_) / this->window_height_, 0.1, 50,
+                           filament::Camera::Fov::VERTICAL);
 
     editor_controller_ = new EditorController(input_controller_, camera_);
 
-    skybox_ = filament::Skybox::Builder()
-                  .color({0.1, 0.125, 0.25, 1.0})
-                  .build(*filament_engine_);
+    skybox_ = filament::Skybox::Builder().color({0.1, 0.125, 0.25, 1.0}).build(*filament_engine_);
     scene_->setSkybox(skybox_);
     view_->setPostProcessingEnabled(false);
 
@@ -146,17 +175,15 @@ ecstasy::app::app(std::string _app_name, std::uint32_t _window_width,
         filament::VertexBuffer::Builder()
             .vertexCount(4)
             .bufferCount(1)
-            .attribute(filament::VertexAttribute::POSITION, 0,
-                       filament::VertexBuffer::AttributeType::FLOAT3)
+            .attribute(filament::VertexAttribute::POSITION, 0, filament::VertexBuffer::AttributeType::FLOAT3)
             /*             .attribute(filament::VertexAttribute::TANGENTS,
                1, filament::VertexBuffer::AttributeType::SHORT4)
                         .normalized(filament::VertexAttribute::TANGENTS) */
             .build(*filament_engine_);
 
-    vertexBuffer->setBufferAt(
-        *filament_engine_, 0,
-        filament::VertexBuffer::BufferDescriptor(
-            vertices, vertexBuffer->getVertexCount() * sizeof(vertices[0])));
+    vertexBuffer->setBufferAt(*filament_engine_, 0,
+                              filament::VertexBuffer::BufferDescriptor(
+                                  vertices, vertexBuffer->getVertexCount() * sizeof(vertices[0])));
     /*     vertexBuffer->setBufferAt(
             *filament_engine_, 1,
             filament::VertexBuffer::BufferDescriptor(
@@ -166,10 +193,8 @@ ecstasy::app::app(std::string _app_name, std::uint32_t _window_width,
     filament::IndexBuffer* indexBuffer =
         filament::IndexBuffer::Builder().indexCount(6).build(*filament_engine_);
 
-    indexBuffer->setBuffer(
-        *filament_engine_,
-        filament::IndexBuffer::BufferDescriptor(
-            indices, indexBuffer->getIndexCount() * sizeof(uint32_t)));
+    indexBuffer->setBuffer(*filament_engine_, filament::IndexBuffer::BufferDescriptor(
+                                                  indices, indexBuffer->getIndexCount() * sizeof(uint32_t)));
 
     filamat::MaterialBuilder::init();
     filamat::MaterialBuilder builder;
@@ -177,11 +202,8 @@ ecstasy::app::app(std::string _app_name, std::uint32_t _window_width,
     filamat::Package package = builder.build(filament_engine_->getJobSystem());
 
     filament::Material* material =
-        filament::Material::Builder()
-            .package(package.getData(), package.getSize())
-            .build(*filament_engine_);
-    material->setDefaultParameter("baseColor", filament::RgbType::LINEAR,
-                                  filament::math::float3{0, 1, 0});
+        filament::Material::Builder().package(package.getData(), package.getSize()).build(*filament_engine_);
+    material->setDefaultParameter("baseColor", filament::RgbType::LINEAR, filament::math::float3{0, 1, 0});
     material->setDefaultParameter("metallic", 0.0f);
     material->setDefaultParameter("roughness", 0.4f);
     material->setDefaultParameter("reflectance", 0.5f);
@@ -193,8 +215,7 @@ ecstasy::app::app(std::string _app_name, std::uint32_t _window_width,
     utils::Entity light = utils::EntityManager::get().create();
 
     filament::LightManager::Builder(filament::LightManager::Type::SUN)
-        .color(filament::Color::toLinear<filament::ACCURATE>(
-            filament::sRGBColor(0.98f, 0.92f, 0.89f)))
+        .color(filament::Color::toLinear<filament::ACCURATE>(filament::sRGBColor(0.98f, 0.92f, 0.89f)))
         .intensity(150'000)
         //.direction({0.7, -1, -0.8})
         .direction({0, 0, 5})
@@ -208,8 +229,7 @@ ecstasy::app::app(std::string _app_name, std::uint32_t _window_width,
     filament::RenderableManager::Builder(1)
         .boundingBox({{-1, -1, -1}, {1, 1, 1}})
         .material(0, materialInstance)
-        .geometry(0, filament::RenderableManager::PrimitiveType::TRIANGLES,
-                  vertexBuffer, indexBuffer, 0, 6)
+        .geometry(0, filament::RenderableManager::PrimitiveType::TRIANGLES, vertexBuffer, indexBuffer, 0, 6)
         .culling(false)
         .build(*filament_engine_, renderable);
     scene_->addEntity(renderable);
@@ -219,20 +239,13 @@ void ecstasy::app::setClearColor(const Eigen::Vector4d& _clear_color) noexcept {
     clear_color_ = _clear_color;
 };
 
-bool ecstasy::app::shouldClose() const noexcept {
-    return glfwWindowShouldClose(window_);
-}
+bool ecstasy::app::shouldClose() const noexcept { return glfwWindowShouldClose(window_); }
 
-ecstasy::InputController* ecstasy::app::getInputController() noexcept {
-    return input_controller_;
-}
+ecstasy::InputController* ecstasy::app::getInputController() noexcept { return input_controller_; }
 
-ecstasy::EditorController* ecstasy::app::getEditorController() noexcept {
-    return editor_controller_;
-}
+ecstasy::EditorController* ecstasy::app::getEditorController() noexcept { return editor_controller_; }
 
-template <ecstasy::AnimationTime T>
-typename T::rep ecstasy::app::getLastAnimationTime() const noexcept {
+template <ecstasy::AnimationTime T> typename T::rep ecstasy::app::getLastAnimationTime() const noexcept {
     return std::chrono::duration_cast<T>(last_animation_time_).count();
 }
 template std::chrono::nanoseconds::rep
@@ -241,8 +254,7 @@ template std::chrono::microseconds::rep
 ecstasy::app::getLastAnimationTime<std::chrono::microseconds>() const noexcept;
 template std::chrono::milliseconds::rep
 ecstasy::app::getLastAnimationTime<std::chrono::milliseconds>() const noexcept;
-template std::chrono::seconds::rep
-ecstasy::app::getLastAnimationTime<std::chrono::seconds>() const noexcept;
+template std::chrono::seconds::rep ecstasy::app::getLastAnimationTime<std::chrono::seconds>() const noexcept;
 
 void printUsage(std::chrono::steady_clock::duration _duration) {}
 
